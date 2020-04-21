02d9f7876bb20d616dffc0ccf6505388fcb65a7a
sgagliano <gsarah6221@gmail.com>
2020-04-20 21:31:43 -0400

    removed lowercase alleles

--- Fileprep.sh
+++ Fileprep.sh
@@ -12,7 +12,6 @@ mv ${i}.txt ${i}v2.txt
 awk '!seen[$1]++' ${i}v2.txt > ${i}.txt
 #To identify the non-ambiguous predictors, use
 awk < ${i}.txt '( ($2=="A"&&$3=="C") || ($2=="A"&&$3=="G") || ($2=="C"&&$3=="A") || ($2=="C"&&$3=="T") || ($2=="G"&&$3=="A") || ($2=="G"&&$3=="T") || ($2=="T"&&$3=="C") || ($2=="T"&&$3=="G") ){print $1}' > ${i}.nonamb
-#if lowercase alleles, switch to lowercase in the above command
 #To identify predictors inside the MHC, and those which individually explain >1% of phenotypic variance, or are in LD with predictor that is
 #i.e. `$5 / ($5 + $6) > 1 / 100` =>  `$5 * 100 > $5 + $6`  => `$5 * 99 > $6` => `$5 > $6 / 99`
 awk < ${i}.txt '(NR>1 && $5>$6/99){print $1}' > ${i}.big
