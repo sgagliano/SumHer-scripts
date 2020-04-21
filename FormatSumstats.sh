a9966c28e8745c6072a3ffbe8938affd50e85be8
sgagliano <gsarah6221@gmail.com>
2020-04-20 21:29:26 -0400

    added lowercase alleles to upper conversion

--- FormatSumstats.sh
+++ FormatSumstats.sh
@@ -6,6 +6,12 @@ gunzip -c ../sumstats/${i}.txt.gz | awk '(NR>1){snp=$3;a1=$5;a2=$4;dir=$10;stat=
 
 #i=AAO
 gunzip -c ../sumstats/${i}.txt.gz | awk '(NR>1){snp=$3;a1=$4;a2=$5;dir=$10;stat=($10/$11)^2;n=17415}(NR==1){print "Predictor A1 A2 Direction Stat n"}(NR>1 && (a1=="a"||a1=="c"||a1=="g"||a1=="t") && (a2=="a"||a2=="c"||a2=="g"||a2=="t")){print snp, a1, a2, dir, stat, n}' > ${i}.txt
+cut -d " " -f 2,3 AAO.txt > AAO.alleles.lower
+tr '[:lower:]' '[:upper:]' < AAO.alleles.lower > AAO.alleles.upper
+cut -d " " -f 1 AAO.txt > AAO.chr
+cut -d " " -f 4,5,6 AAO.txt > AAO.rest
+paste AAO.chr AAO.alleles.upper AAO.rest > AAO.txt
+perl -p -i -e 's/\t/ /g' AAO.txt
 
 #i=PD
 gunzip -c ../sumstats/${i}.txt.gz | awk '(NR>1){snp=$3;a1=$4;a2=$5;dir=$7;stat=($7/$8)^2;n=($10+$11)}(NR==1){print "Predictor A1 A2 Direction Stat n"}(NR>1 && (a1=="A"||a1=="C"||a1=="G"||a1=="T") && (a2=="A"||a2=="C"||a2=="G"||a2=="T")){print snp, a1, a2, dir, stat, n}' > ${i}.txt
