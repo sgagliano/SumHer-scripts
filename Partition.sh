fa9e48c0ac406459cccc1ff93bdee2427b10d486
sgagliano <gsarah6221@gmail.com>
2020-04-22 06:36:53 -0400

    soft coded annot number

--- Partition.sh
+++ Partition.sh
@@ -1,13 +1,14 @@
 i=$1 #trait, e.g. PD, AD, or AOO
 k=$2 #annot_directory
+n=$3 #annotation number, e.g. 53
 
 cd input-${i}
 mkdir ${k}
 cd ${k}
 
 #create tagging file
-#--annotation-number 53; replace 53 with whatever number of files you have correpsonding to ../../test_annotations/${k}/ann_snps.* ((--annotaiton-prefix)
-sumher/ldak5.linux --calc-tagging sumanns --bfile 1000G_EUR_Phase3_plink/1000G.EUR.QC --weights ../sumsect_${i}/weights.short --power -0.25 --extract ../${i}.nonamb --exclude ../${i}.exclude --window-cm 1 --annotation-number 53 --annotation-prefix ../../test_annotations/${k}/ann_snps.
+#--annotation-number correpsonds to number of annotation files: ../../test_annotations/${k}/ann_snps.* ((--annotation-prefix)
+sumher/ldak5.linux --calc-tagging sumanns --bfile 1000G_EUR_Phase3_plink/1000G.EUR.QC --weights ../sumsect_${i}/weights.short --power -0.25 --extract ../${i}.nonamb --exclude ../${i}.exclude --window-cm 1 --annotation-number ${n} --annotation-prefix ../../test_annotations/${k}/ann_snps.
 #can only use --background YES when specifying "partitions"
 
 #regress test statistics onto tagging file
