212da515843e8f6e93265d104853dd54c58ec1c2
sgagliano <gsarah6221@gmail.com>
2020-04-22 06:34:39 -0400

    removed part that goes to Partition.sh

--- PartitionPrep.sh
+++ PartitionPrep.sh
@@ -18,9 +18,3 @@ done
 #Merge weightings across chromosomes
 mkdir sumsect_${i}
 cat sumsect{1..22}/weights.short > sumsect_${i}/weights.short
-
-#create tagging file
-sumher/ldak5.linux --calc-tagging sumanns --bfile 1000G_EUR_Phase3_plink/1000G.EUR.QC.$j --weights sumsect_${i}/weights.short --power -0.25 --extract ${i}.nonamb --exclude ${i}.exclude --window-cm 1 --annotation-number 24 --annotation-prefix ../test_annotations/ann_snps.
-
-#regress test statistics onto taagging file
-sumher/ldak5.linux --sum-hers sumanns --tagfile sumanns.tagging --summary ${i}.txt --check-sums NO
